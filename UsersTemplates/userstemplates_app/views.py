baa66f3a967c5b3be374bae2a262b75b695e693f
DerekS <16406596+drsparks68@users.noreply.github.com>
2021-03-05 13:24:00 -0800

    Almost done
    Everything is done but the users table isn't showing on the main page.  Also need to do a little CSS work to start getting used to it

--- UsersTemplates/userstemplates_app/views.py
+++ UsersTemplates/userstemplates_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import User
 
-# Create your views here.
+# show all of the data from a table
+def index(request):
+    context = {
+    	"all_the_users": User.objects.all()
+    }
+    return render(request, "index.html", context)
+
+def process_user(request):
+    if request.method == 'POST':
+        User.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], email=request.POST['email'], age=request.POST['age'])
+    return render(request, "index.html")
